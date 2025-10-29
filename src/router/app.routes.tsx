7e856f7e4bc40cec1f5c166712233650bae00b1d
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-10-29 19:18:37 +0100

    creacion de layouts para las paginas de Hero y Admin

--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,4 +1,6 @@
+import { AdminLayout } from "@/admin/admin/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { SearchPage } from "@/heroes/pages/search/SearchPage";
@@ -7,19 +9,31 @@ import { createBrowserRouter, Navigate } from "react-router";
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/heroes/1",
-    element: <HeroPage />,
-  },
-  {
-    path: "/search",
-    element: <SearchPage />,
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "heroes/1",
+        element: <HeroPage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+    ],
   },
   {
     path: "/admin",
-    element: <AdminPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
   },
   {
     path: "*",
