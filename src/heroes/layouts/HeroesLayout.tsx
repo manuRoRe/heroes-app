9648f9e6e9eebbee046c993c4576688c8e892484
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-10-29 19:25:37 +0100

    learning the lazy load to not charge all the web

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export const HeroesLayout = () => {
   return (
-    <div className="bg-red-500">
-      <Outlet />
+    <div className="bg-red-100">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/heroes/1">Hero</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+      </ul>
+      <section className="mt-40">
+        <Outlet />
+      </section>
     </div>
   );
 };
--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -3,8 +3,11 @@ import { AdminPage } from "@/admin/pages/AdminPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
+import { lazy } from "react";
+//import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { createBrowserRouter, Navigate } from "react-router";
+
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
