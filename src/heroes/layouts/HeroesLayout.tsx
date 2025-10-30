f1122c1ac8f0778cb1aa5384b678e24ee5af09f1
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-10-30 10:45:13 +0100

    add the HeroesLayout and the customHeader called CustomJumbotron

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,22 +1,11 @@
-import { Link, Outlet } from "react-router";
+import { Outlet } from "react-router";
 
 export const HeroesLayout = () => {
   return (
-    <div className="bg-red-100">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/heroes/1">Hero</Link>
-        </li>
-        <li>
-          <Link to="/search">Search</Link>
-        </li>
-      </ul>
-      <section className="mt-40">
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-purple-50">
+      <div className="max-w-7xl mx-auto p-6">
         <Outlet />
-      </section>
+      </div>
     </div>
   );
 };
