7e5f59ef13e70cb391f778d58047d96bcfa97ba9
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-10-31 11:14:04 +0100

    implementation of tanstack query to call the api one time instead of everytime the components is opened

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -6,16 +6,23 @@ import { HeroGrid } from "@/heroes/components/HeroGrid";
 import { useEffect, useState } from "react";
 import { CustomPagination } from "@/components/custom/CustomPagination";
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
-import { getHeroesByPage } from "@/heroes/actions/get-heroes-by-page.action";
+import { getHeroesByPageAction } from "@/heroes/actions/get-heroes-by-page.action";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomePage = () => {
   const [activeTab, setActiveTab] = useState<
     "all" | "favorites" | "heroes" | "villains"
   >("all");
 
-  useEffect(() => {
-    getHeroesByPage().then((heroes) => console.log({ heroes }));
-  }, []);
+  /* useEffect(() => {
+    getHeroesByPage().then();
+  }, []); */
+
+  const { data } = useQuery({
+    queryKey: ["heroes"],
+    queryFn: getHeroesByPageAction,
+    staleTime: 1000 * 60 * 5, //! 5 minutes
+  });
 
   return (
     <>
