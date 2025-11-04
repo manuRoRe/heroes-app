6e7ec7fc56a9401ff1a59e60c530762394b1fa8d
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-04 12:34:36 +0100

    visual update of the cardHeader in HeroGridCard

--- src/heroes/components/HeroGridCard.tsx
+++ src/heroes/components/HeroGridCard.tsx
@@ -13,11 +13,11 @@ interface Props {
 export const HeroGridCard = ({ hero }: Props) => {
   return (
     <Card className="group overflow-hidden hover:shadow-xl transition-all duration-300 hover:-translate-y-1 bg-gradient-to-br from-white to-gray-50">
-      <div className="relative h-64 overflow-hidden">
+      <div className="relative h-64">
         <img
           src={hero.image}
           alt={hero.alias}
-          className="object-cover transition-all duration-500 group-hover:scale-110"
+          className="object-cover transition-all duration-500 group-hover:scale-110 absolute top-[-30px] w-full h-[410px]"
         />
 
         {/* Status indicator */}
@@ -60,7 +60,7 @@ export const HeroGridCard = ({ hero }: Props) => {
         </Button>
       </div>
 
-      <CardHeader className="pb-3">
+      <CardHeader className="py-3 z-10 bg-gray-100/50 backdrop-blur-sm relative top-1 group-hover:top-[-10px] transition-all duration-300">
         <div className="flex justify-between items-start">
           <div className="space-y-1">
             <h3 className="font-bold text-lg leading-tight">{hero.alias}</h3>
