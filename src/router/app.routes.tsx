b3b8f9649ac2582e0f5e4c9171315037a4c35d9f
Manuel Romero <115268570+mromrey587@users.noreply.github.com>
2025-11-13 12:04:01 +0100

    final commit

--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -5,11 +5,12 @@ import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { lazy } from "react";
 //import { SearchPage } from "@/heroes/pages/search/SearchPage";
-import { createBrowserRouter, Navigate } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 
 const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
-export const appRouter = createBrowserRouter([
+//export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
   {
     path: "/",
     element: <HeroesLayout />,
